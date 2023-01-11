d776461bfdf1b893be54706adba6d677a094bee1
Lina0220 <linuaman2017@gmail.com>
2023-01-11 20:30:54 +1400

    add

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,4 @@
-export function taskFirst() {
-  const task = 'I prefer const when I can.';
-  return task;
-}
 
-export function getLast() {
-  return ' is okay';
-}
+TaskFirst: ${taskFirst()}`);
 
-export function taskNext() {
-  let combination = 'But sometimes let';
-  combination += getLast();
-
-  return combination;
-}
+console.log(`TaskNext: ${taskNext()}`);
